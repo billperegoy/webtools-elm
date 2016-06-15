7d2d2cf8d596a748680b54ce9e48cb941038fbba
Bill Peregoy <bill@billperegoy.com>
2016-06-15 12:40:46 -0400

    Added HTTP error back to RegressionSummary page.

--- src/RegressionSummary.elm
+++ src/RegressionSummary.elm
@@ -7,15 +7,20 @@ import RunTypeSummary exposing (..)
 
 view props =
   div
-    [ class "all-summaries" ]
+    []
     [
       div
-        [ class "summary-container" ]
-        [ (RunTypeSummary.view props.compileSummary) ]
-    , div
-        [ class "summary-container" ]
-        [ (RunTypeSummary.view props.lintSummary) ]
-    , div
-        [ class "summary-container" ]
-        [ (RunTypeSummary.view props.simSummary) ]
+        [ class "all-summaries" ]
+        [
+          div
+            [ class "summary-container" ]
+            [ (RunTypeSummary.view props.compileSummary) ]
+        , div
+            [ class "summary-container" ]
+            [ (RunTypeSummary.view props.lintSummary) ]
+        , div
+            [ class "summary-container" ]
+            [ (RunTypeSummary.view props.simSummary) ]
+        ]
+    , div [] [ text props.errors ]
     ]
